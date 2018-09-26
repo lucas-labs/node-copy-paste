ad39c624aacd1c3c76d8cb83d80dad33b256a3b1
Gabe Marshall <gabemarshall@me.com>
2018-09-26 10:46:52 -0400

    Modified the win32 paste method to use in-line powershell instead of a vbs script. This allows for compilation with the pkg library

--- platform/win32.js
+++ platform/win32.js
@@ -1,22 +1,27 @@
 var iconv = require("iconv-lite");
 var path = require("path");
 
-var vbsPath = path.join(__dirname, ".\\fallbacks\\paste.vbs");
+var pshPaste = "\"[Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes((((new-object -ComObject 'htmlfile').parentWindow.clipboardData.getData('Text') -replace '`r|`n',''))))\""
 
-var paste = { command: "cscript", args: [ "/Nologo", vbsPath ] };
-paste.full_command = [ paste.command, paste.args[0], '"'+vbsPath+'"' ].join(" ");
+var paste = { command: "powershell", args: [ "-c", pshPaste ] };
+paste.full_command = [ paste.command, paste.args[0], pshPaste ].join(" ");
 
 exports.copy = { command: "clip", args: [] };
 exports.paste = paste;
 
-exports.encode = function(str) { return iconv.encode(str, "utf16le"); };
+exports.encode = function(str) { return iconv.encode(str, "utf16le") };
 exports.decode = function(chunks) {
+    
 	if(!Array.isArray(chunks)) { chunks = [ chunks ]; }
 
 	var b64 = iconv.decode(Buffer.concat(chunks), "cp437");
 	b64 = b64.substr(0, b64.length - 2); // Chops off extra "\r\n"
-    
-    // remove bom and decode
-    var result = new Buffer(b64, "base64").slice(3).toString("utf-8");
+    if (typeof Buffer.from === "function"){
+        // Use Node 5.10+ safe method
+        var result = Buffer.from(b64, "base64").toString('utf8');  
+    } else {
+        var result = new Buffer(b64, "base64").toString('utf8');  
+    }
+      
     return result;
 };
